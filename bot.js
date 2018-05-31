a9b97180668ac5f56e1c82c798de6733e401c0dc
FoxTheDev <33805803+FoxTheDev@users.noreply.github.com>
2018-05-31 18:44:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 const fs = require("fs");
 
-let badWords = ["fucking", "slut", "thot", "bitch", "fricky", "cunt", "frack"];
+let badWords = ["fucking", "slut", "thot", "bitch", "fricky", "cunt", "frack", "fuck", "nigger", "spic"];
 
 client.on("ready", () => {
   console.log("I have logged on!");
