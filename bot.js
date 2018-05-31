051af1d86defd1e2b5e021ae1e18d59448fec436
FoxTheDev <33805803+FoxTheDev@users.noreply.github.com>
2018-05-31 14:08:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on("ready", () => {
   console.log("I have logged on!");
 
   console.log(`${client.user.username} has logged in!`);
-  client.user.setActivity(`One Fox Boi`, {type: "playing"});
+  client.user.setActivity('One Foxy Boi', {type: "playing"});
   client.user.setStatus("dnd");
 
 });
